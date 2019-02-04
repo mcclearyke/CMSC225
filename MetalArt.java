56a3717050bb620ccab899ad33b222e4843820dc
mcclearyke <46753320+mcclearyke@users.noreply.github.com>
2019-02-04 14:51:08 -0500

    Add files via upload

--- MetalArt.java
+++ MetalArt.java
@@ -1,11 +1,11 @@
-/******
+/***************************
 * Project 2 - MetalArt
 ****************************
-* This project displayes the area and costs of building a model sculpture based 
-on user input for the size of the pyramids and rectangulare prism
+* This project displays the area and costs of building a model sculpture based 
+on user input for the size of the pyramids and rectangular prism
 *____________________________
 * Kendall McCleary
-* Febuary 3rd, 2019
+* Febuary 4th, 2019
 * CMSC 255 Section 003
 *****/
 
@@ -16,8 +16,8 @@ public class MetalArt {
  public static void printHeading() {
   System.out.println("Kendall McCleary");
   System.out.println("Project 2 - MetalArt");
-  System.out.println("This project displayes the area and costs of building a model sculpture based on user input for the size of the pyramids and rectangulare prism");
-  System.out.println("Febuary 3rd, 2019");
+  System.out.println("This project displays the area and costs of building a model sculpture based on user input for the size of the pyramids and rectangular prism.");
+  System.out.println("Febuary 4th, 2019");
   System.out.println("CMSC 255 Section 003");
   System.out.println();
  }
@@ -27,125 +27,138 @@ public class MetalArt {
 
   		Scanner in = new Scanner(System.in);
 
-  		String name; // input for name
+    // Stating inputs for the Measurements
 
-  		double baseLength1; // value for FIRST pyramid
-  		double heightLength1; // value for FIRST pyramid
-  		
-  		double baseLength2; // value for SECOND pyramid
-  		double heightLength2; // value for SECOND pyramif
-  		
-  		double heightPrism; // value for rectangular prism
-  		double lengthPrism; // value for rectangular prism
-  		double widthPrism; // value for rectangular prism
+  		String name; // input for name
+  		double baseLength1; // Enter value for FIRST pyramid
+  		double heightLength1; // Enter value for FIRST pyramid
+  		double baseLength2; // Enter value for SECOND pyramid
+  		double heightLength2; // Enter value for SECOND pyramid
+  		double heightPrism; // Enter value for rectangular prism
+  		double lengthPrism; // Enter value for rectangular prism
+  		double widthPrism; // Enter value for rectangular prism
+
+    // Stating the inputs for the Calculations
+
+      double areaBase1; // Enter base area value for the FIRST pyramid
+      double areaBase2; // Enter base area value for the SECOND pyramid
+      double slantLength1; // Enter slant length value for the FIRST pyramid
+      double slantLength2; // Enter slant length value for the SECOND pyramid
+      double areaPyramid1; // Enter surface area value for the FIRST pyramid
+      double areaPyramid2; // Enter surface area value for the SECOND pyramid
+      double areaPrism; // Enter surface area value for the Rectangular Prism
+      double totalArea; // Enter total surface area value
+      double totalCost; // Enter total cost vlaue
+      final double COST_PER_INCH = 1.67; // Enter cost per inch constant vlaue
  
-  	// input the name
+  	/************
+    Input the name
+    *************/
 
   		System.out.print("Enter the name of the pyramid: ");
   			name = in.nextLine();
   		
 
-  	// input the vales of the first pyramid
+  	// Input the vales of the first pyramid
 
-  		// input the length of the FIRST pyramid
+  		/***********************************
+      Input the length of the FIRST pyramid
+      ************************************/
 
-  		System.out.print("Enter the length of the base of the first pyramid (inches): "); // length
-  			baseLength1 = in.nextInt();
+  		System.out.print("Enter the length of the base of the first pyramid (inches): "); 
+  			baseLength1 = in.nextDouble();
 
-  		// input the height of the FIRST pyramid
+  		/***********************************
+      Input the height of the FIRST pyramid
+      ************************************/
 
-  		System.out.print("Enter the height of the first pyramid (inches): "); // height
-  			heightLength1 = in.nextInt();
+  		System.out.print("Enter the height of the first pyramid (inches): "); 
+  			heightLength1 = in.nextDouble();
 
   		
-  	// input the values of the second pyramid
+  	// Input the values of the second pyramid
 
-  		// input the length of the SECOND pyramid
+  		/************************************
+      Input the length of the SECOND pyramid
+      *************************************/
 
-  		System.out.print("Enter the length of the base of the second pyramid (inches): "); // length
-  			baseLength2 = in.nextInt();
+  		System.out.print("Enter the length of the base of the second pyramid (inches): "); 
+  			baseLength2 = in.nextDouble();
 
-  		// input the height of the SECOND pyramid
+  		/*************************************
+      Input the height of the SECOND pyramid
+      *************************************/
 
-  		System.out.print("Enter the height of the second pyramid (inches): "); // height
-  			heightLength2 = in.nextInt();
+  		System.out.print("Enter the height of the second pyramid (inches): "); 
+  			heightLength2 = in.nextDouble();
 
 
-  	// input values of the rectangular prism
+  	// Input values of the rectangular prism
 
-  		// input the height 
+  		/***************
+      Input the height 
+      ***************/
 
   		System.out.print("Enter the height of the rectangular prism (inches): "); // height
-  			heightPrism = in.nextInt();
+  			heightPrism = in.nextDouble();
 
-  		// input the length
+  		/***************
+      Input the length
+      ***************/
 
   		System.out.print("Enter the length of the rectangular prism (inches): "); // length
-  			lengthPrism = in.nextInt();
+  			lengthPrism = in.nextDouble();
 
-  		// input the width 
+  		/*************
+      Input the width 
+      **************/
 
   		System.out.print("Enter the width of the rectangular prism (inches): "); // width
-  			widthPrism = in.nextInt();
+  			widthPrism = in.nextDouble();
+ 
 
-  		/******************************************************************
-  		Time for Calculations:
-		
-			Surface Area for the Triangular Pyramids: 
-			
-  			areaPyramid = B + (1/2) * (3*b*l);
-
-  			areaBase = (1/2) * b * Math.sqrt(Math.pow(b,2) - Math.pow(((1/2) * b), 2));
-
-  			slantLength = Math.sqrt(Math.pow(((1/2) * b),2) + Math.pow(h,2));
-		******************************************************************/
-
-		// Starting the Calculations
+		/********************************
+    Output the calculations statement
+    ********************************/
 
 		System.out.println("The " + name + " surface area calculations are: ");
 
-		// Stating the variables needed to do the calculations
 
-		double areaBase1;
-		double areaBase2;
-		double slantLength1;
-		double slantLength2;
-		double areaPyramid1;
-		double areaPyramid2;
-		double areaPrism;
-		double totalArea;
-		double totalCost;
-		final double COST_PER_INCH = 1.67;
+		  /********************************************
+      Finding the surface area of the FIRST pyramid
+      ********************************************/
 
-		// finding the surface area of the FIRST pyramid
+  		areaBase1 = 0.5 * baseLength1 * Math.sqrt(Math.pow(baseLength1, 2) - Math.pow(0.5 * baseLength1, 2)); // Finding B - (1/2) * b * Math.sqrt(Math.pow(b,2) - Math.pow(((1/2) * b), 2))
 
-  		areaBase1 = 0.5 * baseLength1 * Math.sqrt(Math.pow(baseLength1, 2) - Math.pow(0.5 * baseLength1, 2)); // Finding B - the area of the base of the triangle
+  		slantLength1 = Math.sqrt((Math.pow(0.5 * baseLength1, 2)) + (Math.pow(heightLength1, 2))); // Finding l -  Math.sqrt((Math.pow((1/2) * b, 2)) + (Math.pow(h, 2)))
 
-  		slantLength1 = Math.sqrt((Math.pow(0.5 * baseLength1, 2)) + (Math.pow(heightLength1, 2))); // Finding l - the slant length
-
-  		areaPyramid1 = areaBase1 + 0.5 * (3 * baseLength1 * slantLength1); // Finding Surface Area of the triangular pyramid
+  		areaPyramid1 = areaBase1 + 0.5 * (3 * baseLength1 * slantLength1); // Finding Surface Area - B + (1/2) * (3 * b * l)
   			
   			System.out.print("Surface area of the first pyramid: ");
-  			System.out.printf("%5.2f", areaPyramid1);
+  			System.out.printf("%,5.2f", areaPyramid1);
   			System.out.print(" square inches");
   			System.out.println();
 
 
-  		// finding the surface area of the SECOND pyramid
+  	  /*********************************************
+      Finding the surface area of the SECOND pyramid
+      *********************************************/
 
-  		areaBase2 = 0.5 * baseLength2 * Math.sqrt(Math.pow(baseLength2, 2) - Math.pow(0.5 * baseLength2, 2)); // Finding B - the area of the base of the triangle
+  		areaBase2 = 0.5 * baseLength2 * Math.sqrt(Math.pow(baseLength2, 2) - Math.pow(0.5 * baseLength2, 2)); // Finding B - (1/2) * b * Math.sqrt(Math.pow(b,2) - Math.pow(((1/2) * b), 2))
+  		
+      slantLength2 = Math.sqrt((Math.pow(0.5 * baseLength2, 2)) + (Math.pow(heightLength2, 2))); // Finding l -  Math.sqrt((Math.pow((1/2) * b, 2)) + (Math.pow(h, 2)))
 
-  		slantLength2 = Math.sqrt((Math.pow(0.5 * baseLength2, 2)) + (Math.pow(heightLength2, 2))); // Finding l - the slant length
-
-  		areaPyramid2 = areaBase2 + 0.5 * (3 * baseLength2 * slantLength2); // Finding Surface Area of the triangular pyramid
+  		areaPyramid2 = areaBase2 + 0.5 * (3 * baseLength2 * slantLength2); // // Finding Surface Area - B + (1/2) * (3 * b * l)
   			
   			System.out.print("Surface area of the second pyramid: ");
-  			System.out.printf("%5.2f", areaPyramid2);
+  			System.out.printf("%,5.2f", areaPyramid2);
   			System.out.print(" square inches");
   			System.out.println();
 
-  		// finding the surface area of the Rectangular Prism
-  		// surface area = 2lw + 2lh + 2wh
+  		/*************************************************
+      Finding the surface area of the Rectangular Prism
+  		Surface Area = 2lw + 2lh + 2wh
+      *************************************************/
 
   		areaPrism = (2 * lengthPrism * widthPrism) + (2 * lengthPrism * heightPrism) + (2 * widthPrism * heightPrism); // finding the Surface Area of the rectangular prism
 
@@ -154,8 +167,10 @@ public class MetalArt {
   			System.out.print(" square inches");
   			System.out.println();
 
-  		// finding Total Surface Area
-  		// Total SA = areaPyramid1 + areaPyramid2 + areaPrism
+  		/************************************************
+      Finding Total Surface Area
+  		Total SA = areaPyramid1 + areaPyramid2 + areaPrism
+      *************************************************/
 
   		totalArea = areaPyramid1 + areaPyramid2 + areaPrism;
 
@@ -164,7 +179,10 @@ public class MetalArt {
   			System.out.print(" square inches");
   			System.out.println();
 
-  		// claculating the Total Cost
+  		/*****************************
+      Claculate the Total Cost
+      Total Cost = total area * 1.67
+      *****************************/
 
   		totalCost = totalArea * COST_PER_INCH;
 
